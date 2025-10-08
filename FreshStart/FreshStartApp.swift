a981244e52ff584f199056f84879c314d4f27f73
John Symmonds <symmondsj@gmail.com>
2025-10-08 11:27:22 +0200

    Initial Commit

---
+++ FreshStart/FreshStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshStartApp.swift
+//  FreshStart
+//
+//  Created by John Marc Symmonds on 2025/10/08.
+//
+
+import SwiftUI
+
+@main
+struct FreshStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
